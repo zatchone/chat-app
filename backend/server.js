22bc196d9f12432ae268bd06a6d62cc34af9bd25
Yashasvi <sharmayashasvi07@gmail.com>
2024-07-21 03:11:26 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js'
+import path from 'path'
+
 const PORT = process.env.PORT || 5000;
 
+const __dirname =path.resolve();
 dotenv.config();
 
 // Middleware
@@ -19,6 +22,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+});
 // Global error handler
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
